4f0161e3476975618f68e5c50392b0bc98b12d1c
Jack Derksen <jmderksen9@gmail.com>
2024-11-12 23:10:46 -0700

    Fixed project structure (moved all form files into forms directory and removed extra form1.cs).

---
+++ Forms/LoginForm.cs
@@ -0,0 +1,30 @@
+namespace MovieRentalProject
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UserBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PassBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
